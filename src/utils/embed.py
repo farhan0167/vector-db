8c521734f294ef9ae2b24aad6833f5ed78d285d8
farhan0167 <afi225@nyu.edu>
2025-03-28 21:30:34 -0400

    add utils
    - add a knn class and embed function

---
+++ src/utils/embed.py
@@ -0,0 +1,22 @@
+
+import os
+from typing import List
+import cohere
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("COHERE_API_KEY")
+
+co = cohere.ClientV2(
+    api_key=API_KEY
+)
+
+def embed(texts: List[str]) -> List[List[float]]:
+  response = co.embed(
+      texts=texts,
+      model="embed-english-v3.0",
+      input_type="search_document",
+      embedding_types=["float"],
+  )
+  return response.embeddings.float_

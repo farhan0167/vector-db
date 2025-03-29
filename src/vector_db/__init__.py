2386bed3f2b31b699377bdbe84388321a433fa40
farhan0167 <afi225@nyu.edu>
2025-03-29 13:35:14 -0400

    rm unused lines

--- src/vector_db/__init__.py
+++ src/vector_db/__init__.py
@@ -2,4 +2,3 @@ from .database import Database
 from .library import Library
 from .document import Document
 from .chunk import Chunk
-#from ..api.schemas.metadata import DocumentMetadata, ChunkMetadata, LibraryMetadata
